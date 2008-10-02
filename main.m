0058c7556be23f1d112cb8b3e517106a7bbafe91
ashchan <ashchan@gmail.com>
2008-10-03 07:46:24 +0800

    created empty project

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Gmail Notifr
+//
+//  Created by james on 10/3/08.
+//  Copyright (c) 2008 ashchan.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
---
+++ rb_main.rb
@@ -0,0 +1,23 @@
+#
+#  rb_main.rb
+#  Gmail Notifr
+#
+#  Created by james on 10/3/08.
+#  Copyright (c) 2008 ashchan.com. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+def rb_main_init
+  path = OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+  rbfiles = Dir.entries(path).select {|x| /\.rb\z/ =~ x}
+  rbfiles -= [ File.basename(__FILE__) ]
+  rbfiles.each do |path|
+    require( File.basename(path) )
+  end
+end
+
+if $0 == __FILE__ then
+  rb_main_init
+  OSX.NSApplicationMain(0, nil)
+end

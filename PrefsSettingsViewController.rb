270395ed92c8608c15745c3125baf29d94ee902c
James Chen <ashchan@gmail.com>
2009-08-16 21:45:51 +0800

    initial impl of the preferences settings view

--- PrefsSettingsViewController.rb
+++ PrefsSettingsViewController.rb
@@ -10,6 +10,11 @@ require 'osx/cocoa'
 
 class PrefsSettingsViewController <  OSX::NSViewController
 
+  ib_outlet :autoLaunch
+  ib_outlet :showUnreadCount
+  ib_action :saveAutoLaunch
+  ib_action :saveShowUnreadCount
+
   def title
     OSX::NSLocalizedString("PrefsToolbarSettings")
   end
@@ -21,5 +26,19 @@ class PrefsSettingsViewController <  OSX::NSViewController
   def identifier
     "prefsToolbarItemSettings"
   end
+  
+  def loadView
+    super_loadView
+		@autoLaunch.setState(GNPreferences.sharedInstance.autoLaunch? ? NSOnState : NSOffState)
+    @showUnreadCount.setState(GNPreferences.sharedInstance.showUnreadCount? ? NSOnState : NSOffState)
+  end
 
+  def saveAutoLaunch(sender)
+    GNPreferences.sharedInstance.autoLaunch = (@autoLaunch.state == NSOnState)
+  end 
+  
+  def saveShowUnreadCount(sender)
+    GNPreferences.sharedInstance.showUnreadCount = (@showUnreadCount.state == NSOnState)
+    #todo notify the app controller to show/hide menu bar count
+  end
 end

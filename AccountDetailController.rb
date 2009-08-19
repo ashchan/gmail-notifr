f066e28de86c1bda173d3021162da494ade37ccc
James Chen <ashchan@gmail.com>
2009-08-19 20:16:47 +0800

    other binding on account detail view

--- AccountDetailController.rb
+++ AccountDetailController.rb
@@ -9,8 +9,15 @@
 require 'osx/cocoa'
 
 class AccountDetailController < OSX::NSWindowController
-
+  
+  ib_outlet :username
+  ib_outlet :password
+  ib_outlet :interval
+  ib_outlet :accountEnabled
+  ib_outlet :growl
   ib_outlet :soundList
+  
+  
   ib_action :cancel
   ib_action :okay
   
@@ -34,6 +41,12 @@ class AccountDetailController < OSX::NSWindowController
   def awakeFromNib
   	@soundList.removeAllItems
     GNSound.all.each { |s| @soundList.addItemWithTitle(s) }
+		@soundList.selectItemWithTitle(@account.sound)
+ 		@interval.setTitleWithMnemonic(@account.interval.to_s)
+    @accountEnabled.setState(@account.enabled? ? NSOnState : NSOffState)
+		@growl.setState(@account.growl ? NSOnState : NSOffState)
+    @username.setTitleWithMnemonic(@account.username)
+    @password.setTitleWithMnemonic(@account.password)
   end
   
   def cancel(sender)
@@ -41,6 +54,12 @@ class AccountDetailController < OSX::NSWindowController
   end
   
   def okay(sender)
+		@account.sound = @soundList.titleOfSelectedItem
+  	@account.interval = @interval.integerValue
+    @account.enabled = @accountEnabled.state == NSOnState ? true : false
+		@account.growl = @growl.state == NSOnState ? true : false
+    @account.username = @username.stringValue
+    @account.password = @password.stringValue
     @account.save
     
     closeWindow
